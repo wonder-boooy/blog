61d75f631e426516c37b76ced527076ad78c9af1
yuma.wada <yuma.wada.1127@gmail.com>
2021-05-02 06:24:24 +0900

    Add: 新規作成画面追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,26 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
