3e6e9905e04e0a77f211a8076443a3431374ff0d
yuma.wada <yuma.wada.1127@gmail.com>
2021-05-06 07:14:09 +0900

    Add: jsonリクエストに対応するか確認してみた

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
+    respond_to do |format|
+      format.html
+      format.json { render json: { 'data': @articles } }
+    end
   end
 
   def new
